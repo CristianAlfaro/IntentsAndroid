badd042c1555e46b141b5c18eb78ff0ddab83cc0
CristianAlfaro <00167917@uca.edu.sv>
2019-03-22 10:58:43 -0600

    primer comentario

---
+++ app/src/main/java/com/cristianalfaro/taller03_00167917/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.cristianalfaro.taller03_00167917;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.cristianalfaro.taller03_00167917.utils.AppConstant;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private TextView mTextview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        mTextview = findViewById(R.id.tv_message);
+        Intent mIntent = getIntent();
+        if (mIntent != null) {
+            mTextview.setText(mIntent.getStringExtra(Intent.EXTRA_TEXT));
+        }
+    }
+}
---
+++ app/src/main/java/com/cristianalfaro/taller03_00167917/utils/AppConstant.java
@@ -0,0 +1,5 @@
+package com.cristianalfaro.taller03_00167917.utils;
+
+public class AppConstant {
+    public static final String TEXT_KEY = "textKey";
+}
